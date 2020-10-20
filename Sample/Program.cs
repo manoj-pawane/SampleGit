0170706a366aecea08fb39560acb0c9634b23cf1
Manoj <manoj.pawane@bridgelabz.com>
2020-10-20 18:59:23 +0530

    Conflicts resolved

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -9,7 +9,24 @@ namespace Sample
             Console.WriteLine("Welcome to .net ");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Welcome to India");
+<<<<<<< Updated upstream
             Console.WriteLine("Welcome to Universe");
+=======
+            int a = 5;
+            int c = 10;
+            int sumOfTwoNumber = sum(a, c);
+            Console.WriteLine(sumOfTwoNumber);
+
+            int b = 4;
+            int d = 5;
+             sumOfTwoNumber= sum(b, d);
+            Console.WriteLine(sumOfTwoNumber);
+        }
+
+        static int sum(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+>>>>>>> Stashed changes
         }
     }
 }
