2f188c8cd32e8ada3a148f75068100970c0a472c
Manoj <manoj.pawane@bridgelabz.com>
2020-10-20 12:36:57 +0530

    [Manoj] Refactor . Modified Program.cs file added message for .net

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -6,6 +6,7 @@ namespace Sample
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to .net ");
             Console.WriteLine("Hello World!");
         }
     }
